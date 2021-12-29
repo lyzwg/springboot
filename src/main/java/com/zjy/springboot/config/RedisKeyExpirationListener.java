741e1e50f2c97c6d8225de595ff2648006cd013e
lyzwg <1605344679@qq.com>
2021-12-29 09:58:09 +0800

    redis过期键监听

---
+++ src/main/java/com/zjy/springboot/config/RedisKeyExpirationListener.java
@@ -0,0 +1,22 @@
+package com.zjy.springboot.config;
+
+//  created by zjy on 2021/12/29 09:47
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+
+//redis键过期监听
+@Component
+public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+
+    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+        super(listenerContainer);
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        System.out.println("检测到过期的key:" + message.toString());
+    }
+}
---
+++ src/main/java/com/zjy/springboot/config/RedisListenerConfig.java
@@ -0,0 +1,23 @@
+package com.zjy.springboot.config;
+
+//  created by zjy on 2021/12/29 09:41
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+
+@Configuration
+public class RedisListenerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+
+}
